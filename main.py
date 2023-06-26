b4db5d92e5d6317843184f28695f12661e0751b7
Lev <vinnipyx38@gmail.com>
2023-06-26 15:45:39 +0500

    Added multiple sheets using

--- main.py
+++ main.py
@@ -17,18 +17,31 @@ def parse_file(data):
     if not client.login():
         print('Неудачная авторизация, обратитесь к разработчику')
         return
-    result = pd.DataFrame([], columns=['Остаток', 'Продано'])
-    for i, elem in enumerate(data.values):
-        print(f'Парсинг {i + 1}/{len(data.values)}')
-        url = elem[1] + str(elem[2])
-        left, sells = client.parse_url(url)
-        result.loc[len(result.index)] = [left, sells]
+    data_result = []
+    for i, sheet in enumerate(data):
+        print(f"Парсинг листа {i + 1}/{len(data)}")
+        result = pd.DataFrame([], columns=['Остаток', 'Продано'])
+        for j, elem in enumerate(sheet.values):
+            print(f'Парсинг {j + 1}/{len(sheet.values)}')
+            url = elem[1] + str(elem[2])
+            left, sells = client.parse_url(url)
+            result.loc[len(result.index)] = [left, sells]
+            break
+
+        sheet.update(result)
+        data_result.append(sheet)
 
     client.quit()
 
-    data.update(result)
+    return data_result
 
-    return data
+
+def get_data(file):
+    data = []
+    sheet_names = pd.ExcelFile(file).sheet_names
+    for sheet_name in sheet_names:
+        data.append(pd.read_excel(file, sheet_name=sheet_name))
+    return data, sheet_names
 
 
 def main():
@@ -37,11 +50,14 @@ def main():
         print("Excel файл в текущей директории не найден")
         return
 
-    data = pd.read_excel(file)
+    data, sheet_names = get_data(file)
 
     result = parse_file(data)
 
-    result.to_excel(file, index=False)
+    with pd.ExcelWriter(file) as writer:
+        for i, elem in enumerate(result):
+            elem.to_excel(writer, sheet_name=sheet_names[i], index=False)
+
     print('Скрипт успешно завершил работу')
 
 
